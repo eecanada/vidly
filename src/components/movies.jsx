6208ef9289b2e26b60e1267bea25c429ec899766
Eder Canada <canada.eder@gmail.com>
2021-06-23 16:09:53 -0700

    add pagination component interface

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService.js';
+import Pagination from './common/pagination.jsx';
 import Like from './common/like';
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
   };
 
   handleDelete = (movie) => {
@@ -23,6 +25,10 @@ class Movies extends Component {
     });
     this.setState({ movies });
   };
+
+  handlePageChange = page =>{
+    
+  }
 
   render() {
     const { length: count } = this.state.movies;
@@ -71,6 +77,11 @@ class Movies extends Component {
             })}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
