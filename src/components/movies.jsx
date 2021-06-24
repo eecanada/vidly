90060533fe1f0bbe47a33b1b00d6784780a0f741
Eder Canada <canada.eder@gmail.com>
2021-06-23 17:20:09 -0700

    add active class to current page

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,7 +6,8 @@ import Like from './common/like';
 class Movies extends Component {
   state = {
     movies: getMovies(),
-    pageSize: 10,
+    pageSize: 4,
+    currentPage: 1,
   };
 
   handleDelete = (movie) => {
@@ -26,12 +27,13 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
-  handlePageChange = page =>{
-
-  }
+  handlePageChange = (page) => {
+    this.setState({currentPage: page})
+  };
 
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
 
     if (count === 0) return <p> there are no movies in the database </p>;
 
@@ -79,7 +81,8 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
       </React.Fragment>
