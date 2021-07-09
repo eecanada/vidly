1cddb2c3cbf18019b55c809e030413612d676a11
Eder Canada <canada.eder@gmail.com>
2021-07-08 19:10:02 -0700

    search box

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Joi from 'joi-browser';
 import Input from './input';
+import Select from './select';
 
 class Form extends Component {
   state = {
@@ -9,18 +10,16 @@ class Form extends Component {
   };
 
   validate = () => {
-    //result.error
     const options = { abortEarly: false };
     const { error } = Joi.validate(this.state.data, this.schema, options);
-    console.log(error);
     if (!error) return null;
+
     const errors = {};
     for (let item of error.details) errors[item.path[0]] = item.message;
     return errors;
   };
 
   validateProperty = ({ name, value }) => {
-    //input.name input.value
     const obj = { [name]: value };
     const schema = { [name]: this.schema[name] };
     const { error } = Joi.validate(obj, schema);
@@ -29,6 +28,7 @@ class Form extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+
     const errors = this.validate();
     this.setState({ errors: errors || {} });
     if (errors) return;
@@ -37,22 +37,39 @@ class Form extends Component {
   };
 
   handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.error };
+    const errors = { ...this.state.errors };
     const errorMessage = this.validateProperty(input);
     if (errorMessage) errors[input.name] = errorMessage;
     else delete errors[input.name];
+
     const data = { ...this.state.data };
     data[input.name] = input.value;
+
     this.setState({ data, errors });
   };
 
-  renderButton = (label) => {
+  renderButton(label) {
     return (
       <button disabled={this.validate()} className="btn btn-primary">
         {label}
       </button>
     );
-  };
+  }
+
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+
+    return (
+      <Select
+        name={name}
+        value={data[name]}
+        label={label}
+        options={options}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
 
   renderInput(name, label, type = 'text') {
     const { data, errors } = this.state;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,55 +1,59 @@
-import React, { Component } from 'react';
-import { getMovies } from '../services/fakeMovieService.js';
-import Pagination from './common/pagination.jsx';
-import { paginate } from '../utils/paginate';
-import { getGenres } from '../services/fakeGenreService';
-import ListGroup from './common/listGroup.jsx';
-import MoviesTable from './moviesTable.jsx';
-import _ from 'lodash';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import MoviesTable from "./moviesTable";
+import ListGroup from "./common/listGroup";
+import Pagination from "./common/pagination";
+import { getMovies, deleteMovie } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
+import { paginate } from "../utils/paginate";
+import _ from "lodash";
+import SearchBox from "./searchBox";
 
 class Movies extends Component {
   state = {
     movies: [],
-    pageSize: 4,
-    currentPage: 1,
     genres: [],
+    currentPage: 1,
+    pageSize: 4,
+    searchQuery: "",
     selectedGenre: null,
-    hobbies: 'gym',
-    sortColumn: { path: 'title', order: 'asc' },
+    sortColumn: { path: "title", order: "asc" }
   };
 
   componentDidMount() {
-    const genres = [{ _id: '', name: 'All Genres' }, ...getGenres()];
+    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
+
     this.setState({ movies: getMovies(), genres });
   }
 
-  handleDelete = (movie) => {
-    console.log(movie);
-    //m is the this.state array, filter only returns what meets the condition, so here I am returning everything that does not equal to what I currently clicked
-    const movies = this.state.movies.filter((m) => m._id !== movie._id);
-    this.setState({ movies: movies });
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+
+    deleteMovie(movie._id);
   };
 
-  handleLike = (movie) => {
-    let movies = this.state.movies.map((m) => {
-      if (m._id === movie._id) {
-        m.liked = !m.liked;
-      }
-      return m;
-    });
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
     this.setState({ movies });
   };
 
-  handlePageChange = (page) => {
+  handlePageChange = page => {
     this.setState({ currentPage: page });
   };
 
-  handleGenreSelect = (genre) => {
-    this.setState({ selectedGenre: genre, currentPage: 1 });
+  handleGenreSelect = genre => {
+    this.setState({ selectedGenre: genre, searchQuery: "", currentPage: 1 });
   };
 
-  //'title'
-  handleSort = (sortColumn) => {
+  handleSearch = query => {
+    this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 });
+  };
+
+  handleSort = sortColumn => {
     this.setState({ sortColumn });
   };
 
@@ -57,15 +61,19 @@ class Movies extends Component {
     const {
       pageSize,
       currentPage,
-      movies: allMovies,
-      selectedGenre,
       sortColumn,
+      selectedGenre,
+      searchQuery,
+      movies: allMovies
     } = this.state;
 
-    const filtered =
-      selectedGenre && selectedGenre._id
-        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
-        : allMovies;
+    let filtered = allMovies;
+    if (searchQuery)
+      filtered = allMovies.filter(m =>
+        m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+    else if (selectedGenre && selectedGenre._id)
+      filtered = allMovies.filter(m => m.genre._id === selectedGenre._id);
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
@@ -76,9 +84,9 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, sortColumn } = this.state;
+    const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
 
-    if (count === 0) return <p> there are no movies in the database </p>;
+    if (count === 0) return <p>There are no movies in the database.</p>;
 
     const { totalCount, data: movies } = this.getPagedData();
 
@@ -91,19 +99,23 @@ class Movies extends Component {
             onItemSelect={this.handleGenreSelect}
           />
         </div>
-
         <div className="col">
-          <p> There are {totalCount} movies in the database</p>
-
+          <Link
+            to="/movies/new"
+            className="btn btn-primary"
+            style={{ marginBottom: 20 }}
+          >
+            New Movie
+          </Link>
+          <p>Showing {totalCount} movies in the database.</p>
+          <SearchBox value={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
-         
             movies={movies}
+            sortColumn={sortColumn}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
             onSort={this.handleSort}
-            sortColumn={sortColumn}
           />
-
           <Pagination
             itemsCount={totalCount}
             pageSize={pageSize}
