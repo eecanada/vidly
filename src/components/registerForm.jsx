f1e6e88644a137e185f247bed60dc8339882b6ee
Eder Canada <canada.eder@gmail.com>
2021-07-19 10:14:59 -0700

    handle register errors

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import Joi from "joi-browser";
-import Form from "./common/form";
+import React from 'react';
+import Joi from 'joi-browser';
+import Form from './common/form';
+import * as userService from '../services/userService';
 
 class RegisterForm extends Form {
   state = {
-    data: { username: "", password: "", name: "" },
-    errors: {}
+    data: { username: '', password: '', name: '' },
+    errors: {},
   };
 
   schema = {
-    username: Joi.string()
-      .required()
-      .email()
-      .label("Username"),
-    password: Joi.string()
-      .required()
-      .min(5)
-      .label("Password"),
-    name: Joi.string()
-      .required()
-      .label("Name")
+    username: Joi.string().required().email().label('Username'),
+    password: Joi.string().required().min(5).label('Password'),
+    name: Joi.string().required().label('Name'),
   };
 
-  doSubmit = () => {
-    // Call the server
-    console.log("Submitted");
+  doSubmit = async () => {
+    try {
+      await userService.register(this.state.data);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
@@ -32,10 +32,10 @@ class RegisterForm extends Form {
       <div>
         <h1>Register</h1>
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput("username", "Username")}
-          {this.renderInput("password", "Password", "password")}
-          {this.renderInput("name", "Name")}
-          {this.renderButton("Register")}
+          {this.renderInput('username', 'Username')}
+          {this.renderInput('password', 'Password', 'password')}
+          {this.renderInput('name', 'Name')}
+          {this.renderButton('Register')}
         </form>
       </div>
     );
@@ -43,4 +43,3 @@ class RegisterForm extends Form {
 }
 
 export default RegisterForm;
-
