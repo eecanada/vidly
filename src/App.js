15443792f6e5320892b144609a5bdcc0ab18653f
Eder Canada <canada.eder@gmail.com>
2021-09-07 16:25:26 -0700

    change app.js and protectedRoute

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import Movies from '../src/components/movies';
-import NavBar from './components/common/navBar';
-import Customers from './components/customers';
-import Rentals from './components/rentals';
-import NotFound from './components/notFound';
-import MovieForm from './components/movieForm';
-import LoginForm from './components/loginForm';
-import RegisterForm from './components/registerForm';
-import Logout from './components/logout';
-import auth from './services/authService';
-import ProtectedRoute from './components/common/protectedRoute';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/common/navBar.jsx";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout";
+import ProtectedRoute from "./components/common/protectedRoute";
+import auth from "./services/authService";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 class App extends Component {
   state = {};
+
   componentDidMount() {
     const user = auth.getCurrentUser();
     this.setState({ user });
@@ -24,6 +25,7 @@ class App extends Component {
 
   render() {
     const { user } = this.state;
+
     return (
       <React.Fragment>
         <ToastContainer />
@@ -36,7 +38,7 @@ class App extends Component {
             <ProtectedRoute path="/movies/:id" component={MovieForm} />
             <Route
               path="/movies"
-              render={(props) => <Movies {...props} user={this.state.user} />}
+              render={props => <Movies {...props} user={this.state.user} />}
             />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
--- src/components/common/protectedroute.jsx
+++ src/components/common/protectedroute.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import auth from '../../services/authService';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import auth from "../../services/authService";
 
 const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={(props) => {
+      render={props => {
         if (!auth.getCurrentUser())
           return (
             <Redirect
-              to={{ pathname: '/login', 
-                    state: { from: props.location } 
+              to={{
+                pathname: "/login",
+                state: { from: props.location }
               }}
             />
           );
